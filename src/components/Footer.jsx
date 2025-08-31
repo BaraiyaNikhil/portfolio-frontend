06b3c1ea796c264ab69a059a4a5641aa8fb649e0
Baraiya Nikhil <baraiyanikhil593@gmail.com>
2025-08-31 15:22:47 +0530

    updated the resume

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import logo from "../assets/imgs/logo.png";
-import resume from "../assets/files/BaraiyaNikhilResume.pdf";
+import resume from "../assets/files/Resume_Nikhil_Baraiya.pdf";
 
 const Footer = () => {
   const messages = [
